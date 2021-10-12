259f9c5df68783a9d7a27ed4728850021c3ba312
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-10-12 12:43:09 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const PORT = process.env.PORT;
+
+http
+	.createServer((req, res) => {
+		let addr = req.url;
+		let q = url.parse(addr, true);
+		let filePath = '';
+    
+		fs.appendFile('log.txt', `URL: ${addr}, \n Timestamp: ${new Date()} \n\n`, 
+    err => {
+      err ? console.log(err) : console.log('Added to log.');
+		});
+    
+    addr.includes('documentation')
+      ? (filePath = `documentation.html`)
+      : (filePath = `index.html`);
+
+      fs.readFile(filePath, (err, data) => {
+        if(err) throw err;
+
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        res.end()
+      })
+	})
+	.listen(PORT);
+
+console.log(`My first Node test server is running on Port ${PORT}`);
