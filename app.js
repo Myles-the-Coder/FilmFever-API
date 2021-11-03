5b0019a6cb13459a18e75e6041619ddabac7b465
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-11-03 11:52:45 -0600

    Changed CORS permission

--- app.js
+++ app.js
@@ -22,20 +22,22 @@ mongoose
 	.then(res => console.log('DB Connected!'))
 	.catch(err => console.log(err, err.message));
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://m.media-amazon.com'];
 
-app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: (origin, callback) => {
+// 			if (!origin) return callback(null, true);
+// 			if (allowedOrigins.indexOf(origin) === -1) {
+// 				let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
+// 				return callback(new Error(message), false);
+// 			}
+// 			return callback(null, true);
+// 		},
+// 	})
+// );
+app.use(cors())
+
 
 app.use(passport.initialize());
 app.use(json());
