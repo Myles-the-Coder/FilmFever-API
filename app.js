de6fb0742e7332d10a6128b3b6bd8ea6aa86bda9
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-11-03 12:38:21 -0600

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 import express, { json, urlencoded } from 'express';
-import http from 'http'
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import passport from 'passport';
@@ -56,8 +55,6 @@ app.use((req, res, err, next) => {
 		console.error(err.stack);
 		res.status(500).send('Something broke!');
 	}
-  res.removeHeader("Cross-Origin-Resource-Policy")
-  res.removeHeader("Cross-Origin-Embedder-Policy")
 	next();
 });
 
