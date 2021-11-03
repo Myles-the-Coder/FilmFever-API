088b9ecde0828d0d5b4f38c3bb7dd9fc00b7261f
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-11-03 16:43:11 -0600

    Changed HTTP Headers

--- app.js
+++ app.js
@@ -24,21 +24,24 @@ mongoose
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://m.media-amazon.com'];
 
-app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: (origin, callback) => {
+// 			if (!origin) return callback(null, true);
+// 			if (allowedOrigins.indexOf(origin) === -1) {
+// 				let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
+// 				return callback(new Error(message), false);
+// 			}
+// 			return callback(null, true);
+// 		},
+// 	})
+// );
+
+app.use(cors())
 
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Resource-Policy", "cross-origin")
+  res.removeHeader("Cross-Origin-Resource-Policy")
+  res.removeHeader("Cross-Origin-Embedder-Policy")
 next()
 })
 
